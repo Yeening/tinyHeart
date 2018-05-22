7880be4948a73e574236f6144fcd43f85395e6cd
Eclear <eclearning@outlook.com>
2018-05-22 11:36:28 +0800

    Make fruits grow and float in random speed

--- js/fruit.js
+++ js/fruit.js
@@ -5,6 +5,8 @@ var fruitObj = function () {
     this.alive = []; //bool
     this.x = [];
     this.y = [];
+    this.l = [];
+    this.spd = [];    //speed of grow and float
     this.orange = new Image();
     this.blue = new Image();
 }
@@ -23,13 +25,26 @@ fruitObj.prototype.draw = function () {
     for(var i = 0; i < this.num;i++){
         //draw
         //find an ane, grow, fly up
-        ctx2.drawImage(this.orange, this.x[i] - this.orange.width * 0.5,this.y[i] - this.orange.height * 0.5);
+        if(this.alive[i]){
+            if(this.l[i] <= 14){
+                this.l[i] += this.spd[i] * deltaTime;
+            }
+            else{
+                this.y[i] -= this.spd[i] * 7 * deltaTime;
+            }
+            if(this.y[i] < 0){
+                this.alive[i] = false;
+            }
+            ctx2.drawImage(this.orange, this.x[i] - this.l[i] * 0.5,this.y[i] - this.l[i] * 0.5, this.l[i], this.l[i]);
+        }
     }
 }
 fruitObj.prototype.born = function (i) {
     var aneID = Math.floor(Math.random() * ane.num);
     this.x[i] = ane.x[aneID];
     this.y[i] = canHeight - ane.len[aneID];
+    this.l[i] = 0;
+    this.spd[i] = Math.random() * 0.01 +0.005; //[0.005. 0.015)
 }
 
 fruitObj.prototype.update = function () {
